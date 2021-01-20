d0ac8dd7ce5ecdf0ddcb004ae9691c1bdb7ad2f8
Nel-sokchhunly <60089527+Nel-sokchhunly@users.noreply.github.com>
2021-01-20 22:41:09 +0700

    Add Donation card

--- src/views/Donation.vue
+++ src/views/Donation.vue
@@ -3,10 +3,6 @@
     <p class="text-h3 py-5">Support us!</p>
     <p class="text-subtitle-1"></p>
     <v-layout justify-center>
-      <!-- aba card -->
-      <!-- <v-progress-linear v-model="money" height="25">
-        <strong>${{ Math.ceil(money) }}/2000</strong>
-      </v-progress-linear> -->
       <v-card
         max-width="400px"
         dark
@@ -23,8 +19,26 @@
         </div>
       </v-card>
     </v-layout>
-    <!-- target bar -->
-
+    <!-- Donations money cards -->
+    <v-layout row wrap justify-center>
+      <v-card
+        max-width="300"
+        elevation="15"
+        class="ma-2"
+        color="blue-grey lighten-1"
+      >
+        <v-card-title
+          class="justify-center white--text text-h2 donation-card-header font-weight-medium"
+        >
+          5$
+        </v-card-title>
+        <v-divider></v-divider>
+        <v-card-text class="custom-color text-body-1 white--text">
+          you get to blah blah blah blah blah blah blah blah blah blah blah blah
+          blah blah blah blah blah blah blah blah blah blah
+        </v-card-text>
+      </v-card>
+    </v-layout>
     <!-- Supporter list -->
     <p class="text-h4 pt-5">Our supporters</p>
     <v-chip>Your name can be next!</v-chip>
@@ -42,5 +56,11 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.donation-card-header {
+  padding: 1.5em;
+}
+.custom-color {
+  background-color: #16576d;
+}
 </style>
