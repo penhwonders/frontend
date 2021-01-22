4b8031425176e6e77953a090b2f54b04f0ac0ddd
munyoudoum <pav.munyoudoum19@kit.edu.kh>
2021-01-22 19:53:11 +0700

    vue-analytics

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router/router';
 import store from './plugins/appState'
-
+import VueAnalytics from 'vue-analytics';
+//and then use it in main.js
+Vue.use(VueAnalytics, {
+    id: 'UA-186949747-1',
+    router
+})
 Vue.config.productionTip = false
 
 new Vue({
