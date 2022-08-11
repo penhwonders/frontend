295bd2809be7a940db764387994c6cedd31acc1d
munyoudoum <munyoudoum@gmail.com>
2022-08-12 04:45:33 +0700

    fix: use env var for firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,10 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
+    apiKey: process.env.VUE_APP_API_KEY,
+    databaseURL: process.env.VUE_APP_DATABASE_URL,
+    projectId: process.env.VUE_APP_PROJECT_ID,
 };
 
 // Initialize Firebase
